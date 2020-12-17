172c2bca040467be4b270d912031041bd10abc28
Francisco Javier Trujillo Mata <fran.t.es@sync.money>
2020-12-17 22:01:38 +0100

    Change GCC build configuration

--- scripts/002-gcc-stage1.sh
+++ scripts/002-gcc-stage1.sh
@@ -12,44 +12,51 @@ else
 	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} || exit 1
 fi
 
+TARGET_ALIAS="iop" 
+TARGET="iop"
+
 OSVER=$(uname)
 ## Apple needs to pretend to be linux
 if [ ${OSVER:0:6} == Darwin ]; then
 	TARG_XTRA_OPTS="--build=i386-linux-gnu --host=i386-linux-gnu"
-elif [ ${OSVER:0:10} == MINGW64_NT ]; then
-	TARG_XTRA_OPTS="--build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32"
 else
 	TARG_XTRA_OPTS=""
 fi
 
 ## Determine the maximum number of processes that Make can work with.
-if [ ${OSVER:0:5} == MINGW ]; then
-	PROC_NR=$NUMBER_OF_PROCESSORS
-elif [ ${OSVER:0:6} == Darwin ]; then
-	PROC_NR=$(sysctl -n hw.ncpu)
-else
-	PROC_NR=$(nproc)
-fi
+PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
-echo "Building with $PROC_NR jobs"
+## Create and enter the toolchain/build directory
+mkdir build-$TARGET-stage1 && cd build-$TARGET-stage1 || { exit 1; }
 
-## For each target...
-for TARGET in "iop"; do
-	## Create and enter the build directory.
-	rm -rf build-$TARGET-stage1 && mkdir build-$TARGET-stage1 && cd build-$TARGET-stage1 || { exit 1; }
+## Configure the build.
+../configure \
+  --quiet \
+  --prefix="$PS2DEV/$TARGET_ALIAS" \
+  --target="$TARGET" \
+  --enable-languages="c" \
+  --with-float=hard \
+  --with-newlib \
+  --disable-nls \
+  --disable-shared \
+  --disable-libssp \
+  --disable-libmudflap \
+  --disable-threads \
+  --disable-libgomp \
+  --disable-libquadmath \
+  --disable-target-libiberty \
+  --disable-target-zlib \
+  --without-ppl \
+  --without-cloog \
+  --with-headers=no \
+  --disable-libada \
+  --disable-libatomic \
+  --disable-multilib \
+  --disable-lto \
+  $TARG_XTRA_OPTS || { exit 1; }
 
-	## Configure the build.
-	if [ ${OSVER:0:6} == Darwin ]; then
-		CC=/usr/bin/gcc CXX=/usr/bin/g++ LD=/usr/bin/ld CFLAGS="-O0 -ansi -Wno-implicit-int -Wno-return-type" ../configure --quiet --prefix="$PS2DEV/$TARGET" --target="$TARGET" --enable-languages="c" --with-newlib --without-headers $TARG_XTRA_OPTS || { exit 1; }
-	else
-		../configure --quiet --prefix="$PS2DEV/$TARGET" --target="$TARGET" --enable-languages="c" --with-newlib --without-headers $TARG_XTRA_OPTS || { exit 1; }
-	fi
-
-	## Compile and install.
-	make --quiet clean && make --quiet -j $PROC_NR CFLAGS="$CFLAGS -O2" LDFLAGS="$LDFLAGS -s" && make --quiet install && make --quiet clean || { exit 1; }
-
-	## Exit the build directory.
-	cd .. || { exit 1; }
-
-	## End target.
-done
+## Compile and install.
+make --quiet -j $PROC_NR clean   || { exit 1; }
+make --quiet -j $PROC_NR all     || { exit 1; }
+make --quiet -j $PROC_NR install || { exit 1; }
+make --quiet -j $PROC_NR clean   || { exit 1; }
